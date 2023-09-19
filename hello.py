7263272a62442a4ba4111eff4cfec89c7b571092
HarisRasul12 <haris.rasul@mail.utoronto.ca>
2023-09-18 20:03:52 -0400

    Activity 4 Second Commit

--- hello.py
+++ hello.py
@@ -15,6 +15,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -33,8 +34,12 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'),email=session.get('email'))
