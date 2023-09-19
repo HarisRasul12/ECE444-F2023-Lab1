1f0a4b10920d9a988f50b085ca81f4692653434a
HarisRasul12 <haris.rasul@mail.utoronto.ca>
2023-09-18 21:34:54 -0400

    ACTIVITY 4 Commit 2 MARK THIS HAS ALL DEPENDANCIES

--- hello.py
+++ hello.py
@@ -15,18 +15,26 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'),email=session.get('email'))
+
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, {}!</h1>'.format(name)
