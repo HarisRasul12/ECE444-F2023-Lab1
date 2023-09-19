1e34f4604992495e846ecac867e92c7dd44a4ac2
HarisRasul12 <haris.rasul@mail.utoronto.ca>
2023-09-18 21:18:54 -0400

    readded activity2 to final piece

--- hello.py
+++ hello.py
@@ -19,15 +19,6 @@ class NameForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -43,3 +34,7 @@ def index():
         session['email'] = form.email.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'),email=session.get('email'))
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
